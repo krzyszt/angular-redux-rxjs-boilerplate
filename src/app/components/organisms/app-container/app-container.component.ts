cf39b67828de51258a90b4113f3a78066a1003f7
Krzysztof Kaminski <krzysztof.kaminski@piksel.com>
2017-12-22 11:39:43 +0000

    app container

---
+++ src/app/components/organisms/app-container/app-container.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthActions } from '../../../store/actions/auth.actions';
+
+@Component({
+  selector: 'app-app-container',
+  templateUrl: './app-container.component.html',
+  styleUrls: ['./app-container.component.css']
+})
+export class AppContainerComponent implements OnInit {
+
+  constructor(private authActions: AuthActions, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  onLogout() {
+    this.authActions.logout();
+    this.router.navigate(['/login']);
+  }
+}
