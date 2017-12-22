83b03f775ee938af9f2e79fd62d0492c44256161
Krzysztof Kaminski <krzysztof.kaminski@piksel.com>
2017-12-22 12:36:23 +0000

    tests

--- src/app/components/organisms/app-container/app-container.component.spec.ts
+++ src/app/components/organisms/app-container/app-container.component.spec.ts
@@ -1,14 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 
 import { AppContainerComponent } from './app-container.component';
+import { AuthActions } from '../../../store/actions/auth.actions';
+
+class RouterStub {
+  navigate(url: string) { return url; }
+}
 
 describe('AppContainerComponent', () => {
   let component: AppContainerComponent;
   let fixture: ComponentFixture<AppContainerComponent>;
+  const authActionsStub = {
+    authenticated: true,
+    user: { name: 'Test User'}
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AppContainerComponent ]
+      declarations: [ AppContainerComponent ],
+      providers:    [
+        { provide: AuthActions, useValue: authActionsStub },
+        { provide: Router, useClass: RouterStub }
+      ]
     })
     .compileComponents();
   }));
--- src/app/components/pages/login-page/login-page.component.spec.ts
+++ src/app/components/pages/login-page/login-page.component.spec.ts
@@ -1,20 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 
 import { LoginPageComponent } from './login-page.component';
 import { AuthActions } from '../../../store/actions/auth.actions';
 
+class RouterStub {
+  navigate(url: string) { return url; }
+}
+
 describe('LoginPageComponent', () => {
   let component: LoginPageComponent;
   let fixture: ComponentFixture<LoginPageComponent>;
-  let authActionsStub = {
-    isLoggedIn: true,
+  const authActionsStub = {
+    authenticated: true,
     user: { name: 'Test User'}
   };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginPageComponent ],
-      providers:    [ {provide: AuthActions, useValue: authActionsStub } ]
+      providers:    [
+        { provide: AuthActions, useValue: authActionsStub },
+        { provide: Router, useClass: RouterStub }
+      ]
     })
     .compileComponents();
   }));
